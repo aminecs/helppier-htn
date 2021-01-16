ab43e9f51ede800974e86628bc51d21f3b63cc05
Teleayo Oyekunle <randomlyinspirational@gmail.com>
2021-01-16 15:15:22 +0100

    Add db support & user model

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,9 +1,35 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+from config import Config
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config.from_object(Config)
+
+    return app
+
+app = create_app()
+
+# DB Config
+db = SQLAlchemy(app)
+
+from .models.user import UserModel
+db.create_all()
+
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    old_user = UserModel.find_by_email("pparker@spidey.com")
+    if old_user:
+        return jsonify({
+            "msg": "User {} already exists".format(old_user.email)
+        })
+    new_user = UserModel(firstname="Peter", lastname="Parker", email="pparker@spidey.com")
+    new_user.save()
+    return jsonify(new_user.json())
 
 if __name__ == '__main__':
     app.run()
---
+++ backend/app/models/user.py
@@ -0,0 +1,44 @@
+from cockroachdb.sqlalchemy import run_transaction
+from sqlalchemy.orm import sessionmaker
+
+from ..app import db
+
+class UserModel(db.Model):
+    __tablename__ = "user"
+
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(50))
+    lastname = db.Column(db.String(50))
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(80))
+    rewards = db.Column(db.Numeric(10,2))
+
+    def __repr__(self):
+        return f"<User({self.id}, {self.firstname}, {self.lastname}, {self.email}, {self.password})"
+
+
+    # def save(self):
+    #     def callback(session):
+    #         session.add(self)
+    #     print("Saving user")
+    #     run_transaction(sessionmaker(bind=db.engine), callback)
+    #     print("Saved user")
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def json(self):
+        return {
+            "firstname": self.firstname,
+            "lastname": self.lastname,
+            "email": self.email,
+        }
+
+    @classmethod
+    def find_by_email(cls, email):
+        return cls.query.filter(cls.email==email).first()
+
+    @classmethod
+    def find_by_id(cls, email):
+        return cls.query.filter(cls.id==_id).first()
